bcf3810218282ab3c45a0fbe4abedac5fe4b94c8
Denys Aristides <denysloira@gmail.com>
2021-09-13 18:03:43 +0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Denysloira/projeto-sds3" target="_blank" rel="noreferrer">Denys Aristides</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="www.linkedin.com/in/denys-aristides-4156a8140" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
